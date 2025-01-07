e881f193f07d550cb4fb9a598714ed56a6674b16
Juan <cruzjuan298@gmail.com>
2025-01-07 01:59:11 -0500

    change frontend to include input file and post button

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const app = express();
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("Testing");
-});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
