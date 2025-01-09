09f6da4643947229ec9327dfb2097a4930fefe80
Juan <cruzjuan298@gmail.com>
2025-01-08 22:42:38 -0500

    feat: update frontend support to gather input file data

--- public/index.js
+++ public/index.js
@@ -1,26 +1,93 @@
+import logger from "./logger.js"
+
 const responseContainer = document.getElementById("response-div");
 const scraperButton = document.getElementById("post");
+const fileInput = document.getElementById("file-input")
+
+let fileData = "";
 
 scraperButton.addEventListener("click", displayScraped)
+
+//reseting input value when page is refreshed
+window.addEventListener("load", () => {
+    fileInput.value = "";
+})
 
 function displayScraped(e){
     e.preventDefault();
     responseContainer.textContent = "";
+
     if (responseContainer.classList.contains("hidden")) {
         responseContainer.classList.remove("hidden");
         responseContainer.classList.add("visible");
     }
-    createScrapeHTML();
+
+    createScrapeHTML(fileData);
 }
 
-function createScrapeHTML(){
+function gettingInputData() {
+    const file = fileInput.files[0];
+    
+    if (file) {
+        try {
+            const reader = new FileReader();
+
+            reader.onload = async (event) => {
+                const typedArray = new Unit8Array(event.target.result);
+                const pdf = await pdfjsLib.getDocument(typedArray).promise;
+
+                let extractedText = "";
+                for (let i = 1; i < pdf.numPages; i++) {
+                    const page = await pdf.getPage(i);
+                    const textContent = await page.getTextContent();
+                    textContent.items.forEach(item => extractedText += item.str + " ");
+                }
+
+                if (extractedText.trim().length > 0) {
+                    sendToServer({ text: extractedText });
+                    console.log("Sent text to server")
+                } else {
+                    uploadRawPdf(file);
+                }
+            };
+            reader.readAsArrayBuffer(file);
+        } catch (error) {
+            logger.error("Error extracting text: ". error);
+            uploadRawPdf(file);
+        }
+    }
+}
+
+function sendToServer(data) {
+    fetch("/process-text", {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(data),
+    }).then(response => response.json())
+      .then(result => console.log("Server response:", result))
+      .catch(error => logger.error("Error sending text to server:", error ));
+}
+
+function uploadRawPdf(data) {
+    const formData = new FormData();
+    formData.append("file", data);
+
+    fetch("/process-pdf", {
+        method: POST,
+        body: formData,
+    }).then(response => response.json())
+      .then(result => console.log("Server response:", result))
+      .catch(error => logger.error("Error uploading PDF to server", error));
+}
+
+function createScrapeHTML(textFromFile){
     const h1Element = document.createElement("h1");
     h1Element.id = "response-h1";
     h1Element.textContent = "Scraped Text:";
 
     const responseH2 = document.createElement("h2");
     responseH2.id = "response-h1";
-    responseH2.textContent = "Dates Scraped";
+    responseH2.textContent = textFromFile;
 
     responseContainer.appendChild(h1Element);
     responseContainer.appendChild(responseH2);
--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 
 const port = 3000
 
+app.post("/process-text", (req, res) => {
+    const { text } = req.body;
+
+    console.log("Recieved text:", text);
+    res.json({ success: true, message: "Text processed succesfully"})
+})
+
+/*app.post("/process-pdf", (req, res) => {
+
+})
+*/
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
